79958b82513dc1748194f9397ec204d299a8d26d
Shanjit Singh Jajmann <shanjitsingh@gmail.com>
2016-06-09 22:34:05 -0500

    updated README

---
+++ build/bsp_stk_leds.d
@@ -0,0 +1,146 @@
+build/bsp_stk_leds.o: \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/kits/common/bsp/bsp_stk_leds.c \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/em_device.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg222f32.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/CMSIS/Include/arm_math.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/CMSIS/Include/core_cm0plus.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/CMSIS/Include/core_cmInstr.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/CMSIS/Include/core_cmFunc.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/CMSIS/Include/core_cm0plus.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/system_efm32zg.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_aes.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_dma_ch.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_dma.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_msc.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_emu.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_rmu.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_cmu.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_timer_cc.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_timer.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_acmp.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_usart.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_prs_ch.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_prs.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_idac.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_gpio_p.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_gpio.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_vcmp.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_adc.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_leuart.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_pcnt.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_i2c.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_rtc.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_wdog.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_dma_descriptor.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_devinfo.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_romtable.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_calibrate.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_prs_signals.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_dmareq.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_dmactrl.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_af_ports.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_af_pins.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/emlib/inc/em_cmu.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/emlib/inc/em_bitband.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/emlib/inc/em_gpio.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/emlib/inc/em_assert.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/kits/common/bsp/bsp.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/kits/EFM32ZG_STK3200/config/bspconfig.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/kits/common/bsp/bsp_bcp.h \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/emlib/inc/em_usart.h
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/em_device.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg222f32.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/CMSIS/Include/arm_math.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/CMSIS/Include/core_cm0plus.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/CMSIS/Include/core_cmInstr.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/CMSIS/Include/core_cmFunc.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/CMSIS/Include/core_cm0plus.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/system_efm32zg.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_aes.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_dma_ch.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_dma.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_msc.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_emu.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_rmu.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_cmu.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_timer_cc.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_timer.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_acmp.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_usart.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_prs_ch.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_prs.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_idac.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_gpio_p.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_gpio.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_vcmp.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_adc.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_leuart.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_pcnt.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_i2c.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_rtc.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_wdog.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_dma_descriptor.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_devinfo.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_romtable.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_calibrate.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_prs_signals.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_dmareq.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_dmactrl.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_af_ports.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/Device/SiliconLabs/EFM32ZG/Include/efm32zg_af_pins.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/emlib/inc/em_cmu.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/emlib/inc/em_bitband.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/emlib/inc/em_gpio.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/emlib/inc/em_assert.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/kits/common/bsp/bsp.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/kits/EFM32ZG_STK3200/config/bspconfig.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/kits/common/bsp/bsp_bcp.h:
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/emlib/inc/em_usart.h:
---
+++ build/em_assert.d
@@ -0,0 +1,5 @@
+build/em_assert.o: \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/emlib/src/em_assert.c \
+ /home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/emlib/inc/em_assert.h
+
+/home/cuil/Downloads/SimplicityStudio_v2/developer/sdks/efm32/v2/emlib/inc/em_assert.h:
